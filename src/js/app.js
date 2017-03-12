5c8556ea2d5f18fea452d65905deb21ac89b40ff
Alexander Rettner <alexander.rettner@googlemail.com>
2017-03-12 10:34:56 +0100

    Table gets filled using a template; Added a pagination element

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
-//App js
-console.log('it works');
+(function($, window, document, undefined) {
+
+  'use strict';
+
+  $(function() {
+    console.log('it works');
+
+    var template = $('#consumption-row-template').html();
+    var consumptionTable = $('#consumption-table');
+    for (var i = 0; i < 3; i++) {
+      consumptionTable.append(template);
+    }
+
+  });
+
+})(jQuery, window, document);
