5602e1b9baa88a4779b8a4f5b204a0cd52a20e37
Alexander Rettner <alexander.rettner@googlemail.com>
2017-03-14 18:51:37 +0100

    Moved table-update to its own function and created a template for pagination.

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,10 @@
 
   'use strict';
 
+  var VISIBLE_ROWS = 10;
+  var model = { bills: [] };
+
   $(function() {
-    var consumptionTable = $('#consumption-table');
-    var source = $("#consumption-row-template").html();
-    var template = Handlebars.compile(source);
 
     Handlebars.registerHelper("dateFormat", function(date, formatOption) {
       var format = formatOption || 'LLLL';
@@ -28,11 +28,46 @@
           bill.pricePerLiter = bill.price / bill.quantity;
           return bill;
         });
-      var substitute = template({ bills: bills });
-      consumptionTable.find('tbody').replaceWith(substitute);
+      model.bills = bills;
+      updateView({ currentPage: 0 });
+    });
 
+    $('#page-navigation').on("click", ".pagination li.page-item", function(e) {
+      updateView({ currentPage: 1 })
     });
 
   });
+
+  function updateView(data) {
+    updateTable(data);
+    updatePagination(data);
+  }
+
+  function updateTable(data) {
+    var consumptionTable = $('#consumption-table');
+    var source = $("#consumption-row-template").html();
+    var template = Handlebars.compile(source);
+
+    var totalPages = Math.ceil(model.bills.length / VISIBLE_ROWS);
+    var bills = model.bills.slice(data.currentPage * VISIBLE_ROWS, (data.currentPage + 1) * VISIBLE_ROWS);
+
+    var substitute = template({ bills: bills });
+    consumptionTable.find('tbody').replaceWith(substitute);
+  }
+
+  function updatePagination(data) {
+    var pageNavigation = $('#page-navigation');
+    var source = $("#pagination-template").html();
+    var template = Handlebars.compile(source);
+
+    var pages = [];
+    var totalPages = Math.ceil(model.bills.length / VISIBLE_ROWS);
+    for (var i = 0; i < totalPages; i++) {
+      pages.push({ display: i + 1, active: i == data.currentPage });
+    }
+
+    var substitute = template({ pages: pages });
+    pageNavigation.find('ul').replaceWith(substitute);
+  }
 
 })(jQuery, window, document);
